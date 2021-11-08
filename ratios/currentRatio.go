ee81eaf96774b8259fc4964df1e53b96a618641f
tscales <tscales8791@gmail.com>
2021-11-07 20:02:50 -0800

    currentRatio

---
+++ ratios/currentRatio.go
@@ -0,0 +1,15 @@
+package ratios
+
+// CurrentRatio divides a company's current assets buy their current liaibilites.
+//
+// A current ratio > 1 means that a company is capable of paying off its short term debts.
+// Current assets: cash or inventory a company wishes to turn in to cash
+// in the next 12 months.
+//
+// Current liabilities: debts that must be repaid within 12 months.
+// this can include loans, rent, payroll, and taxes.
+
+// You can find these numbers on a company's balance sheet
+func CurrentRatio(assets float32, liabilities float32) float32 {
+	return assets / liabilities
+}
