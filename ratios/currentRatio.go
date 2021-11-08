7bca6e0dc481bf54f1cdd79051ddc741382a7704
tscales <tscales8791@gmail.com>
2021-11-07 20:11:52 -0800

    update doc

--- ratios/currentRatio.go
+++ ratios/currentRatio.go
@@ -3,6 +3,7 @@ package ratios
 // CurrentRatio divides a company's current assets buy their current liaibilites.
 //
 // A current ratio > 1 means that a company is capable of paying off its short term debts.
+//
 // Current assets: cash or inventory a company wishes to turn in to cash
 // in the next 12 months.
 //
